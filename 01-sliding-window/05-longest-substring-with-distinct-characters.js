62eb36e669f9ec033b43b9493e1299a48b40835a
Roger David Cruz <roger.david.cruz@gmail.com>
2022-04-21 18:01:10 -0400

    sliding window - longest substring with distinct characters

--- 01-sliding-window/05-longest-substring-with-distinct-characters.js
+++ 01-sliding-window/05-longest-substring-with-distinct-characters.js
@@ -1,5 +1,22 @@
 /*
 Longest Substring With Distinct Characters
+
+Given a string, find the length of the longest substring, which has all distinct characters.
+
+Example 1:
+  Input: String="aabccbb"
+  Output: 3
+  Explanation: The longest substring with distinct characters is "abc".
+
+Example 2:
+  Input: String="abbbb"
+  Output: 2
+  Explanation: The longest substring with distinct characters is "ab".
+
+Example 3:
+  Input: String="abccde"
+  Output: 3
+  Explanation: Longest substrings with distinct characters are "abc" & "cde".
 */
 
 /*
@@ -8,28 +25,28 @@ grokking - the index were the last char last appeared
 
        s
  "abcdeafjyk"
-             e
+          e
+
+Time: O(N)             
+Space: O(1)
 */
 
-const longestSubString = string => {
+const longestSubString = str => {
     let start = 0,
-        maxLength = 0,
+        maxLen = 0,
         charIdxMap = {};
   
-    for (end = 0; end < string.length; end++) {
-        const rightChar = string[end];
+    for (end = 0; end < str.length; end++) {
+      const rightChar = str[end];
       if (rightChar in charIdxMap) {
-        windowStart = Math.max(windowStart, charIdxMap[right] + 1)
-      } 
-
-      charIdxMap[rightChar] = windowEnd;
-
-      maxLength = Math.max(maxLength, end - start + 1);  
+        start = Math.max(start, charIdxMap[rightChar] + 1);
+      }
+      charIdxMap[rightChar] = end;
+      maxLen = Math.max(maxLen, end - start + 1);  
     }
-  
-    return maxLength;
+    return maxLen;
   }
   
-console.log(longestSubString("aabccbb")) //3
-console.log(longestSubString("abbbb")) //2
-console.log(longestSubString("abcdeafjyk")) //9
+console.log(longestSubString("aabccbb"), 'expecting: 3') //3
+console.log(longestSubString("abbbb"), 'expecting: 2') //2
+console.log(longestSubString("abcdeafjyk"), 'expecting: 9') //9
