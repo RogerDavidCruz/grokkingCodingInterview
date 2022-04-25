6e06169800d19c959109faf79ee8ead31d446192
Roger David Cruz <roger.david.cruz@gmail.com>
2022-04-25 17:58:43 -0400

    two pointers - sort colors - dutch flag

--- 02-two-pointers/08-dutch-national-flag.js
+++ 02-two-pointers/08-dutch-national-flag.js
@@ -73,3 +73,34 @@ const dutchFlagSort = arr => {
     }
     return arr;
 }
+
+/*
+time: O(n)
+space: O(1)
+
+*/
+
+//LEETCODE Version LC 75, SORT COLORS:
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ const sortColors = nums => {
+    let left = 0,
+        right = nums.length - 1,
+        i = 0;
+    
+    while (i <= right) {
+        if (nums[i] === 0) {
+            [nums[i], nums[left]] = [nums[left], nums[i]];
+            left++
+            i++
+        } else if (nums[i] === 2) {
+            [nums[i], nums[right]] = [nums[right], nums[i]]
+            right--
+        } else {
+            i++
+        }
+    }
+    return nums;
+};
